aa7e199b2eb00410f2c837b0d8392dc7a0d090ed
emmagon-z <emmagonz@umich.edu>
2025-09-17 00:04:23 -0400

    deleted:    fullsubmit.tar.gz 	modified:   main.cpp 	new file:   test-1-apple-apply-splw.txt 	deleted:    test-1-go-went-qlw.txt 	new file:   test-14-same-same-qcw.txt 	new file:   test-15-a-e-scw.txt 	deleted:    test-15-team-tem-qldm.txt 	deleted:    test-3-ape-man-scw.txt 	new file:   test-3-stared-star-qlm.txt 	renamed:    test-14-at-a-qlw.txt -> test-4-a-an-qlw.txt 	deleted:    test-4-noon-moon-spw.txt 	new file:   test-6-form-from-spm.txt 	deleted:    test-6-stone-sone-qlm.txt 	deleted:    test-7-a-c-qclw.txt 	new file:   test-7-bot-bet-qcw.txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Project Identifier: 50EB44D3F029ED934858FFFCEAC3547C68251FC9
 
 #include "letter.h"
-#include <unordered_map>
 using namespace std;
 
 int main (int argc, char* argv[]) {
@@ -47,24 +46,28 @@ int main (int argc, char* argv[]) {
         
         string t = trim(line);
         if (t.empty()) {
-            break;
+            continue;
         }
         if (t.rfind("//", 0) == 0) {
             continue;
         }
         if (dictType == 'S') {
             if (!opts.allowLength && t.length() != opts.beginWord.length()) {
-                continue;
+                // skip words of incorrect length
             }
-            dictionary.push_back({t});
+            else {
+                dictionary.push_back({t});
+            }
         }
         else {
             auto exp = expand_complex_line(t);
             for (auto &w : exp) {
                 if (!opts.allowLength && w.length() != opts.beginWord.length()) {
-                    continue;
+                    // skip words of incorrect length
                 }
-                dictionary.push_back({w});
+                else {
+                    dictionary.push_back({w});
+                }
             }
         }
         ++read_count;
@@ -88,11 +91,7 @@ int main (int argc, char* argv[]) {
         exit(1);
     }
 
-    unordered_map<size_t, vector<size_t>> buckets;
-    for (size_t i = 0; i < dictionary.size(); ++i) {
-        buckets[dictionary[i].word.size()].push_back(i);
-    }
-
+    // search algorithm
     deque<size_t> sc;
     dictionary[beginIndex].discovered = true;
     dictionary[beginIndex].parent = SIZE_MAX;
@@ -118,57 +117,41 @@ int main (int argc, char* argv[]) {
         }
 
         const string &curWord = dictionary[current].word;
-        size_t curLen = curWord.size();
-        
-        vector<size_t> candidateLengths;
-        candidateLengths.reserve(3);
-        candidateLengths.push_back(curLen);
-        if (opts.allowLength) {
-            candidateLengths.push_back(curLen + 1);
-            if (curLen > 0) {
-                candidateLengths.push_back(curLen - 1);
-            }
-        }
 
         vector<size_t> neighborsToAdd;
 
-        for (size_t len : candidateLengths) {
-            auto it = buckets.find(len);
-            if (it == buckets.end()) {
+        for (size_t i = 0; i < dictionary.size(); ++i) {
+            if (dictionary[i].discovered) {
                 continue;
             }
-            
-            for (size_t i : it->second) {
-                if (dictionary[i].discovered) {
-                    continue;
-                }
-                const string &candidate = dictionary[i].word;
-                
-                bool similar = false;
-                if (opts.allowSwap && swapAdjacent(curWord, candidate)) {
-                    similar = true;
-                }
-                if (!similar && opts.allowChange && oneLetterChange(curWord, candidate)) {
-                    similar = true;
-                }
-                if (!similar && opts.allowLength && insertOrDelete(curWord, candidate)) {
-                    similar = true;
-                }
 
-                if (similar) {
-                    dictionary[i].discovered = true;
-                    dictionary[i].parent = current;
-                    ++discovered_count;
+            const string &candidate = dictionary[i].word;
 
-                    if (opts.search_mode == SearchMode::QUEUE) {
-                        sc.push_back(i);
-                    }
-                    else {
-                        neighborsToAdd.push_back(i);
-                    }
+            bool similar = false;
+            if (opts.allowChange && oneLetterChange(curWord, candidate)) {
+                similar = true;
+            }
+            if (!similar && opts.allowSwap && swapAdjacent(curWord, candidate)) {
+                similar = true;
+            }
+            if (!similar && opts.allowLength && insertOrDelete(curWord, candidate)) {
+                similar = true;
+            }
+
+            if (similar) {
+                dictionary[i].discovered = true;
+                dictionary[i].parent = current;
+                ++discovered_count;
+
+                if (opts.search_mode == SearchMode::QUEUE) {
+                    sc.push_back(i);
+                }
+                else {
+                    neighborsToAdd.push_back(i);
                 }
             }
         }
+    
         if (opts.search_mode == SearchMode::STACK) {
             for (auto it = neighborsToAdd.rbegin(); it != neighborsToAdd.rend(); ++it) {
                 sc.push_back(*it);
